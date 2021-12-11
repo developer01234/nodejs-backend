1cd8b21158c6011fb960ac47a444b8f5e29398d3
Ruslan <ruslansorokin234567@gmail.com>
2021-12-11 22:53:37 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function start() {
+  const port = 5000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(port, () =>
+    console.log(`Server start been on port ${port}`),
+  );
+}
+
+start();
