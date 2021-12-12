75873c54c371f8829a110f48d5e9b0686f84c105
Ruslan <ruslansorokin234567@gmail.com>
2021-12-12 14:31:53 +0300

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,15 @@ import { UsersModule } from './users/users.module';
 @Module({
 	imports: [
 		ConfigModule.forRoot({
-			envFilePath: '.env',
+			envFilePath: `${process.env.NODE_ENV}.env`,
 		}),
 		SequelizeModule.forRoot({
 			dialect: 'postgres',
-			host: 'localhost',
-			port: 5432,
-			username: 'postgres',
-			password: 'root',
-			database: 'bug-free-lamp',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRESS_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRESS_PASSWORD,
+			database: process.env.POSTGRES_DB,
 			models: [],
 			autoLoadModels: true,
 		}),
